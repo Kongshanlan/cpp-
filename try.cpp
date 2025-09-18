3ab7d39cb6354dd2d7c687fcd28db05e63f47f1f
yangwang <3077028131@qq.com>
2025-09-18 14:38:07 +0800

    first-try

---
+++ try.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+char num1,num2;
+cout<<"Enter two numbers:"<<endl;
+cin>>num1>>num2;
+cout<<"The character you entered is:"<<(char)num1+num2<<endl;
+return 0;
+}
